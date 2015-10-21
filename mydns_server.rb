46f50d9308c1960081185ad8a78f29b3ba98794d
Andras Horvath <mail@log69.com>
2015-10-21 23:22:44 +0200

    allow more characters that domain names use too

--- mydns_server.rb
+++ mydns_server.rb
@@ -25,7 +25,8 @@ loop do
 
 		# get domain name and source ip address
 		# sanitize input strongly
-		name = c.gets[/[\?]*[a-z]+/][0..15]
+		name = c.gets[/[\?]*[\.\-_a-zA-Z0-9]+/][0..100]
+p name
 		ip = c.addr[-1][/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/]
 
 		# question mark makes it a request for name resolution
